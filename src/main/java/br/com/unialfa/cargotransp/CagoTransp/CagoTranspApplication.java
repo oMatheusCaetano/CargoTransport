af21bed891efa073928c5ce141075243e8a15ba6
Matheus Caetano <caetanomatheus876@gmail.com>
2021-04-07 17:21:32 -0300

    Project Skeleton

---
+++ src/main/java/br/com/unialfa/cargotransp/CagoTransp/CagoTranspApplication.java
@@ -0,0 +1,13 @@
+package br.com.unialfa.cargotransp.CagoTransp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagoTranspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CagoTranspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unialfa/cargotransp/CagoTransp/CagoTranspApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unialfa.cargotransp.CagoTransp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagoTranspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
