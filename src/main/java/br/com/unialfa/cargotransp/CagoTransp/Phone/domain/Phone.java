df2083a2c36eaf002fa60668ecf8332192400ae4
Matheus Caetano <caetanomatheus876@gmail.com>
2021-04-07 20:36:14 -0300

    Create Phone Model

---
+++ src/main/java/br/com/unialfa/cargotransp/CagoTransp/Phone/domain/Phone.java
@@ -0,0 +1,13 @@
+package br.com.unialfa.cargotransp.CagoTransp.Phone.domain;
+
+public class Phone {
+
+    private String number;
+
+    public String getNumber() { return this.number; }
+
+    public Phone setNumber(String number) {
+        this.number = number;
+        return this;
+    }
+}
