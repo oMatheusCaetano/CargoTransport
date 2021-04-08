6286be0b4c5abc4fbcf91f6b51fe6c905e205b07
Matheus Caetano <caetanomatheus876@gmail.com>
2021-04-07 21:04:48 -0300

    Created Delivery Repository

---
+++ src/main/java/br/com/unialfa/cargotransp/CagoTransp/Delivery/repository/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package br.com.unialfa.cargotransp.CagoTransp.Delivery.repository;
+
+import br.com.unialfa.cargotransp.CagoTransp.Delivery.domain.Delivery;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeliveryRepository extends CrudRepository<Delivery, Long> {}
